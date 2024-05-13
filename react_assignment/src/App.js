2fafe6552a512e699fd99184ec2b8ff5c25f63a7
Sri Abhishek Mamidi <mamidisriabhishek@gmail.com>
2024-05-14 01:02:29 +0530

    assignment

---
+++ react_assignment/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+const container = document.getElementById('react-container');
+
+function App() {
+  return (
+    <div className="App">
+      Hey Kalvians! Welcome to React Learning
+      Let's rock and roll
+    </div>
+  );
+}
+
+export default App;
---
+++ vite_react_assignment/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1>hello Kalvium</h1>
+    </>
+  )
+}
+
+export default App
